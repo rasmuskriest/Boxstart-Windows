3e779ee89b6bdaad3b5cc437d08e0e3b179bfdb8
rasmuskriest <rasmusk@outlook.com>
2018-10-11 20:48:34 +0300

    Add 1Password

--- Deploy-Machines.ps1
+++ Deploy-Machines.ps1
@@ -21,7 +21,7 @@ $installWindowsFeaturesDismList = @("Microsoft-Windows-Subsystem-Linux", "Micros
 $addWindowsCapabilityList = @("OpenSSH*")
 $installPowerShellToolsList = @("au", "conemu", "gow", "pstools", "vcxsrv")
 $installPowerShellModulesList = @("Get-ChildItemColor", "posh-git", "Pscx", "PSReadline", "z")
-$installPrerequisitesList = @("7zip", "dropbox", "flashplayerplugin", "git", "google-drive-file-stream", "insync", "javaruntime", "jdk8", "lame", "notepad3", "python", "python2", "quicktime", "unchecky")
+$installPrerequisitesList = @("1password", "7zip", "dropbox", "flashplayerplugin", "git", "google-drive-file-stream", "insync", "javaruntime", "jdk8", "lame", "notepad3", "python", "python2", "quicktime", "unchecky")
 $installBrowsersList = @("firefox -packageParameters 'l=en-US'", "GoogleChrome", "tor-browser")
 $installCommunicationToolsList = @("mattermost-desktop", "skype")
 $installDevToolsList = @("android-sdk", "sqlistebrowser", "winscp")
